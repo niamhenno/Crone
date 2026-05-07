a1ae9e8215e11d0120de0a949b6da68ef8cf2de7
Sean Browne <64338141+ShamrockSB@users.noreply.github.com>
2026-05-07 23:39:16 +0100

    Walk behind houses

---
+++ Assets/_Scripts/Other/Crone_Level.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Crone_Level" tilewidth="32" tileheight="32" tilecount="952" columns="34">
+ <grid orientation="orthogonal" width="6" height="6"/>
+ <image source="../../_Art/Textures/image.png" width="1106" height="904"/>
+</tileset>
