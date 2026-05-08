61d61087ef9edd2a42932a59657058d084eb63fd
Sean Browne <64338141+ShamrockSB@users.noreply.github.com>
2026-05-08 19:24:50 +0100

    New pixel art layered aseprite file for scene

--- Assets/_Scripts/Player/Normal/Player_Movement.cs
+++ Assets/_Scripts/Player/Normal/Player_Movement.cs
@@ -6,17 +6,34 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator anim;
+    private Vector3 originalScale;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+
+        if (moveInput.x > 0)
+        {
+                transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+
+
+          
     }
 
     public void OnMove(InputValue value)
